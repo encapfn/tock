d3d2aa47af26082bc2155d90999768298615c2de
Leon Schuermann <leon@is.currently.online>
2024-04-07 19:06:22 -0400

    WIP

--- encapfn-tock/src/lib.rs
+++ encapfn-tock/src/lib.rs
@@ -1,4 +1,10 @@
-#![feature(offset_of)]
+#![feature(
+    offset_of,
+    naked_functions,
+    asm_const,
+    maybe_uninit_as_bytes,
+    maybe_uninit_write_slice
+)]
 
 pub mod binary;
 pub mod rv32i_c_rt;
