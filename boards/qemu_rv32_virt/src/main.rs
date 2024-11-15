e9c9e90646e7638131f63d2750bc48bd887b92a3
Leon Schuermann <leon@is.currently.online>
2024-11-15 10:21:33 -0500

    boards/qemu_rv32_virt: add binary target based on library crate

---
+++ boards/qemu_rv32_virt/src/main.rs
@@ -0,0 +1,68 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2022.
+
+//! Board file for qemu-system-riscv32 "virt" machine type
+
+#![no_std]
+// Disable this attribute when documenting, as a workaround for
+// https://github.com/rust-lang/rust/issues/62184.
+#![cfg_attr(not(doc), no_main)]
+
+use core::ptr::addr_of_mut;
+
+use kernel::debug;
+use kernel::{capabilities, create_capability};
+use qemu_rv32_virt_lib::{self, PROCESSES};
+
+// How should the kernel respond when a process faults.
+const FAULT_RESPONSE: capsules_system::process_policies::PanicFaultPolicy =
+    capsules_system::process_policies::PanicFaultPolicy {};
+
+/// Main function called after RAM initialized.
+#[no_mangle]
+pub unsafe fn main() {
+    // These symbols are defined in the linker script.
+    extern "C" {
+        /// Beginning of the ROM region containing app images.
+        static _sapps: u8;
+        /// End of the ROM region containing app images.
+        static _eapps: u8;
+        /// Beginning of the RAM region for app memory.
+        static mut _sappmem: u8;
+        /// End of the RAM region for app memory.
+        static _eappmem: u8;
+    }
+
+
+    let (board_kernel, platform, chip, _default_peripherals) =
+        qemu_rv32_virt_lib::start();
+
+    // Acquire required capabilities
+    let process_mgmt_capability = create_capability!(capabilities::ProcessManagementCapability);
+    let main_loop_capability = create_capability!(capabilities::MainLoopCapability);
+
+    // ---------- PROCESS LOADING, SCHEDULER LOOP ----------
+
+    kernel::process::load_processes(
+        board_kernel,
+        chip,
+        core::slice::from_raw_parts(
+            core::ptr::addr_of!(_sapps),
+            core::ptr::addr_of!(_eapps) as usize - core::ptr::addr_of!(_sapps) as usize,
+        ),
+        core::slice::from_raw_parts_mut(
+            core::ptr::addr_of_mut!(_sappmem),
+            core::ptr::addr_of!(_eappmem) as usize - core::ptr::addr_of!(_sappmem) as usize,
+        ),
+        &mut *addr_of_mut!(PROCESSES),
+        &FAULT_RESPONSE,
+        &process_mgmt_capability,
+    )
+    .unwrap_or_else(|err| {
+        debug!("Error loading processes!");
+        debug!("{:?}", err);
+    });
+
+    board_kernel.kernel_loop(&platform, chip, Some(&platform.ipc), &main_loop_capability);
+}
